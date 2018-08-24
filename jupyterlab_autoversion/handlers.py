a9b547be4134276689ff85bfbbb607959891593b
Tim Paine <t.paine154@gmail.com>
2018-08-24 11:23:19 -0400

    fix gethandler

--- jupyterlab_autoversion/handlers.py
+++ jupyterlab_autoversion/handlers.py
@@ -12,21 +12,24 @@ class GetHandler(IPythonHandler):
         path = self.get_argument('path', '')
         id = self.get_argument('id', '')
 
-        if id:
-            path = os.path.join(self.repo.git_dir, id)
-            last = os.path.join(path, 'LAST')
-            if os.path.exists(path):
-                if os.path.exists(last):
-                    with open(last, 'r') as fp:
-                        self.finish({'id': id, 'last': fp.read()})
-                else:
-                    self.finish({'id': id, 'last': ''})
-            else:
-                self.finish({'id': id, 'last': ''})
-        else:
+        if not id and not path:
+            self.finish({'id': '', 'last': ''})
+
+        if not id and path:
             sha = hashlib.sha256()
             sha.update(path.encode())
             id = sha.hexdigest()
+
+        path = os.path.join(self.repo.working_tree_dir, id)
+        last = os.path.join(path, 'LAST')
+
+        if os.path.exists(path):
+            if os.path.exists(last):
+                with open(last, 'r') as fp:
+                    self.finish({'id': id, 'last': fp.read()})
+            else:
+                self.finish({'id': id, 'last': ''})
+        else:
             self.finish({'id': id, 'last': ''})
 
 
