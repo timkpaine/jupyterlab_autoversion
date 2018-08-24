e3588b26ff5446a69b8ab895f501c885f66220e3
Tim Paine <t.paine154@gmail.com>
2018-08-23 21:25:48 -0400

    .

--- jupyterlab_autoversion/handlers.py
+++ jupyterlab_autoversion/handlers.py
@@ -1,3 +1,5 @@
+import os
+import os.path
 import uuid
 from notebook.base.handlers import IPythonHandler
 
@@ -7,11 +9,21 @@ class GetHandler(IPythonHandler):
         self.repo = repo
 
     def get(self):
-        notebook = self.get_argument('notebook')
         id = self.get_argument('id', None)
         if id:
-        uuid.uuid4().hex
-        self.finish(notebook)
+            path = os.path.join(self.repo.git_dir, id)
+            last = os.path.join(path, 'LAST')
+            if os.path.exists(path):
+                if os.path.exists(last):
+                    with open(last, 'r') as fp:
+                        self.finish({'id': id, 'last': fp.read()})
+
+                else:
+                    self.finish({'id': id, 'last': ''})
+            else:
+                self.finish({'id': id, 'last': ''})
+        id = uuid.uuid4().hex
+        self.finish({'id': id, 'last': ''})
 
 
 class RestoreHandler(IPythonHandler):
