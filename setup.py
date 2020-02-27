b49871d3e47fd6c9508699eb38fcd2801be9f7fa
Tim Paine <t.paine154@gmail.com>
2020-02-27 14:36:39 -0500

    copypasa

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     packages=find_packages(exclude=['tests', ]),
     install_requires=requires,
     extras_require={
-        'dev': ['pytest', 'pytest-cov', 'pylint', 'flake8', 'bumpversion', 'autopep8', 'mock']
+        'dev': requires + ['pytest', 'pytest-cov', 'pylint', 'flake8', 'bumpversion', 'autopep8', 'mock']
     },
     include_package_data=True,
     zip_safe=False,
