cd7863946b02220865d60217b32a3027219f81f5
Tim Paine <t.paine154@gmail.com>
2022-04-11 11:03:10 -0400

    Bump version: 0.3.5 → 0.3.6

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ builder = npm_builder(
 
 setup(
     name=name,
-    version="0.3.5",
+    version="0.3.6",
     description="Automatically version jupyter notebooks on save",
     long_description=long_description,
     long_description_content_type="text/markdown",
