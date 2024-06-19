8ca0c816bce504a9801277012ed9d1ddb5a8972b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-06-18 22:09:59 -0400

    fix versioning

--- jupyterlab_autoversion/__init__.py
+++ jupyterlab_autoversion/__init__.py
@@ -1,6 +1,6 @@
-from ._version import __version__  # noqa: F401
 from .extension import load_jupyter_server_extension  # noqa: F401
 
+__version__ = "0.4.0"
 
 def _jupyter_server_extension_paths():
     return [{"module": "jupyterlab_autoversion.extension"}]
